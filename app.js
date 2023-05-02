619742d22c8552aa78a5761cc983e395c792b1b7
Aditya Danturthi <aditya2096@gmail.com>
2023-05-02 19:20:46 -0230

    icon change on click

--- app.js
+++ app.js
@@ -2,14 +2,22 @@
 
 let acc = document.getElementsByClassName('question');
 let i;
+let symbol = document.querySelectorAll('i');
+console.log('Symbol', symbol.length);
 
 for (i=0; i<acc.length;i++){
+        let currentSymbol =symbol[i];
         acc[i].addEventListener('click', function(){
                 let answer = this.nextElementSibling;
                 if(answer.style.display === 'block'){
                         answer.style.display='none';
+                        currentSymbol.classList.remove('fa-minus');
+                        currentSymbol.classList.add('fa-plus');
+                        
                 } else {
                         answer.style.display = 'block';
+                        currentSymbol.classList.add('fa-minus');
+                        currentSymbol.classList.remove('fa-plus');
                 }
         });
 }
